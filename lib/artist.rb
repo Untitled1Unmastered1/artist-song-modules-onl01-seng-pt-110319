bdc673aa7e5bd9021035b8b353027805011b6324
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-06 04:28:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,3 @@
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/paramable'
 class Artist
   extend Memorable
   extend Findable 
